f1673f9a55907498cdff7e4c528494f207b12cb7
João Victor <111023680+joaovictordjl@users.noreply.github.com>
2022-08-31 19:47:10 -0300

    Add files via upload

---
+++ aula02/script2_03.js
@@ -0,0 +1,2 @@
+document.write("6 + 8 =<br>");//aspas duplas escrevem/mostram o texto
+document.write(6 + 8);//escreve o cálculo
---
+++ aula03/script3_07.js
@@ -0,0 +1,4 @@
+const num1 = prompt ("Digite um número");
+ const num2 = prompt ("Digite um número");
+ let result = num1 + num2;
+ document.write ("soma = "+result+"<br>");
